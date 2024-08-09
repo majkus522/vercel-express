78b0b50acc93428beef636e84245110dfeb429cd
majkus522 <mikohus522@gmail.com>
2024-08-09 23:15:02 +0200

    a

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const express = require("express");
 
 const app = express();
+injectSpeedInsights();
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
